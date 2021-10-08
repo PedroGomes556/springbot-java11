edc0b8c8d4147f2ca404de12b3d08a75c604132d
Pedro Gomes <you@example.com>
2021-10-07 21:39:49 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/pjappsandgames/course/entities/User.java
+++ src/main/java/com/pjappsandgames/course/entities/User.java
@@ -1,15 +1,20 @@
 package com.pjappsandgames.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
@@ -19,7 +24,10 @@ public class User implements Serializable{
 	private String name;
 	private String email;
 	private String phone;
-	private String password;
+	private String password;	
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		
@@ -73,7 +81,11 @@ public class User implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -90,4 +102,6 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
+
 }
