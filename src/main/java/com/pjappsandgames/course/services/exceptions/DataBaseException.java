15c7cb73e3ce9684f5059498db65d5e8c5046cb4
Pedro Gomes da Silva Júnior <pedrogomesps4xboxone@gmail.com>
2021-10-14 21:44:11 -0300

    Exception Handling - delete

---
+++ src/main/java/com/pjappsandgames/course/services/exceptions/DataBaseException.java
@@ -0,0 +1,12 @@
+package com.pjappsandgames.course.services.exceptions;
+
+public class DataBaseException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	public DataBaseException(String msg) {
+		super(msg);
+	}
+	
+}
